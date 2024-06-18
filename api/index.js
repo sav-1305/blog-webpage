260845b32387f53d1ddf4fe87728c5fdb29d3ea8
sav-1305 <satvikagrawal.86@gmail.com>
2024-06-19 02:29:29 +0530

    added via upload

---
+++ api/index.js
@@ -0,0 +1,142 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const fs = require('fs');
+const Post = require('./models/Post');
+
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asddasdasfaghfjaisf23erasdfbn32';
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+mongoose.connect('mongodb+srv://satvikagrawal86:Q96SivwbESOdNNv0@satvik-86.ugsbjx7.mongodb.net/?retryWrites=true&w=majority&appName=satvik-86');
+
+app.post('/register', async (req, res) => {
+    const {username,password} = req.body;
+    try {
+        const userDoc = await User.create({
+            username, 
+            password:bcrypt.hashSync(password,salt),
+        });
+        res.json(userDoc);
+    } catch(e) {
+        res.status(400).json(e);
+    }
+    
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        //logged in
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id:userDoc._id,
+                username,
+            });
+        });
+        //res.json();
+    } else {
+        //not logged in
+        res.status(400).json('wrong credentials');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    }); 
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+});
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const {originalname, path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const {title, summary, content} = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id,
+        });
+        res.json(postDoc);
+    });
+});
+
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const {originalname, path} = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path+'.'+ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const {id, title, summary, content } = req.body;
+        const postDoc = await Post.findById(id);
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        if (!isAuthor) {
+            return res.status(400).json('You are not the author of this post!');
+        }
+
+        await postDoc.updateOne({
+            title, 
+            summary, 
+            content,
+            cover: newPath ? newPath : postDoc.cover,
+        });
+
+        res.json(postDoc);
+    });
+
+});
+
+app.get('/post', async (req, res) => {
+    res.json(
+        await Post.find()
+            .populate('author', ['username'])
+            .sort({createdAt: -1})
+            .limit(20)
+    );
+});
+
+app.get('/post/:id', async (req, res) => {
+    const {id} = req.params;
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
+});
+
+app.listen(4000);
+
+//mongodb+srv://satvikagrawal86:<password>@satvik-86.ugsbjx7.mongodb.net/?retryWrites=true&w=majority&appName=satvik-86
+//Q96SivwbESOdNNv0
